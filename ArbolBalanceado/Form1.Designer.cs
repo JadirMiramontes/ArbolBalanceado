411ca7f2fb853db88f3f4cb33406497cb70cee28
JadirMiramontes <jadirescuela23@gmail.com>
2024-03-18 18:48:30 -0600

    cambios finales del arbol balanceado

--- ArbolBalanceado/Form1.Designer.cs
+++ ArbolBalanceado/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBoxValue = new TextBox();
+            btnInsert = new Button();
+            listBoxTraversal = new ListBox();
+            labelInstruction = new Label();
+            SuspendLayout();
+            // 
+            // textBoxValue
+            // 
+            textBoxValue.Location = new Point(132, 87);
+            textBoxValue.Name = "textBoxValue";
+            textBoxValue.Size = new Size(100, 23);
+            textBoxValue.TabIndex = 0;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(141, 138);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(75, 55);
+            btnInsert.TabIndex = 1;
+            btnInsert.Text = "Generar Arbol";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // listBoxTraversal
+            // 
+            listBoxTraversal.FormattingEnabled = true;
+            listBoxTraversal.ItemHeight = 15;
+            listBoxTraversal.Location = new Point(382, 12);
+            listBoxTraversal.Name = "listBoxTraversal";
+            listBoxTraversal.Size = new Size(406, 319);
+            listBoxTraversal.TabIndex = 2;
+            // 
+            // labelInstruction
+            // 
+            labelInstruction.AutoSize = true;
+            labelInstruction.Location = new Point(84, 47);
+            labelInstruction.Name = "labelInstruction";
+            labelInstruction.Size = new Size(205, 15);
+            labelInstruction.TabIndex = 3;
+            labelInstruction.Text = "Ingrese los datos que desea balancear";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelInstruction);
+            Controls.Add(listBoxTraversal);
+            Controls.Add(btnInsert);
+            Controls.Add(textBoxValue);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBoxValue;
+        private Button btnInsert;
+        private ListBox listBoxTraversal;
+        private Label labelInstruction;
     }
 }
--- ArbolBalanceado/Form1.cs
+++ ArbolBalanceado/Form1.cs
@@ -1,10 +1,85 @@
+using System;
+using System.Windows.Forms;
+
 namespace ArbolBalanceado
 {
     public partial class Form1 : Form
     {
+        private class TreeNode
+        {
+            public int data;
+            public TreeNode left, right;
+
+            public TreeNode(int data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        private class BalancedTree
+        {
+            private TreeNode root;
+
+            // Función para insertar un nuevo nodo en el árbol balanceado
+            private TreeNode Insert(TreeNode node, int data)
+            {
+                if (node == null)
+                    return new TreeNode(data);
+
+                if (data < node.data)
+                    node.left = Insert(node.left, data);
+                else if (data > node.data)
+                    node.right = Insert(node.right, data);
+
+                return node;
+            }
+
+            // Función para recorrer el árbol en orden
+            private void InorderTraversal(TreeNode node, ListBox listBox)
+            {
+                if (node != null)
+                {
+                    InorderTraversal(node.left, listBox);
+                    listBox.Items.Add(node.data);
+                    InorderTraversal(node.right, listBox);
+                }
+            }
+
+            // Método público para insertar un nuevo nodo en el árbol balanceado
+            public void Insert(int data)
+            {
+                root = Insert(root, data);
+            }
+
+            // Método público para recorrer el árbol en orden
+            public void Traverse(ListBox listBox)
+            {
+                listBox.Items.Clear();
+                InorderTraversal(root, listBox);
+            }
+        }
+
+        private BalancedTree tree;
+
         public Form1()
         {
             InitializeComponent();
+            tree = new BalancedTree();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxValue.Text, out int value))
+            {
+                tree.Insert(value);
+                tree.Traverse(listBoxTraversal);
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor numérico válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
